a143e5820d28f4dbbaf2e0c02e4b5a0260e1e80e
unknown <whiskeytango30@live.com>
2024-05-01 19:03:30 -0700

    added comments

--- dailytasks.ps1
+++ dailytasks.ps1
@@ -1,8 +1,11 @@
 $tempPath = "C:\Windows\Temp\*"
 $spoolerService = "Spooler"
 
+# Checking disk space
 Get-PSDrive 
 
+# Restarting theprint spooler service
 Restart-Service -Name $spoolerService
 
+# Clearing temporary files
 Remove-Item -Path $tempPath -Recurse 
