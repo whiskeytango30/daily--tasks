2e536eee1c35430ef5d14a67312e18c95837dde1
unknown <whiskeytango30@live.com>
2024-05-01 19:16:44 -0700

    defined output

--- dailytasks.ps1
+++ dailytasks.ps1
@@ -2,10 +2,17 @@ $tempPath = "C:\Windows\Temp\*"
 $spoolerService = "Spooler"
 
 # Checking disk space
-Get-PSDrive 
+$diskSpace = Get-PSDrive 
 
 # Restarting theprint spooler service
 Restart-Service -Name $spoolerService
+$spoolerStatus = Get-service -Name $SpoolerService | Select-Object Status
 
 # Clearing temporary files
 Remove-Item -Path $tempPath -Recurse 
+ 
+[PSCustomObject]@{
+    DiskSpace = $diskSpace
+    SpoolerServiceStatus = $spoolerStatus.Status
+    tempFilesCleared = $tempFilesRemaining.Count -eq 0
+}
